6ba3af33e8a7eef79761f70a38040fa43e5416b8
maksimvedeshin <Vedeshinm@gmail.com>
2023-06-27 14:03:40 +1000

    я

--- 1 proect/1 proect.cpp
+++ 1 proect/1 proect.cpp
@@ -40,4 +40,53 @@ int main()
 	}
 	t4 = clock();
 	cout << "Время суммирования массива double: " << t4 - t3 << "\n";
+	const int n2 = 2;
+	float** Bsgl = new float* [n];
+	for (int i = 0; i < n; ++i)
+		Bsgl[i] = new float[n2];
+	float** Csgl = new float* [10];
+	for (int i = 0; i < 10; ++i)
+		Csgl[i] = new float[n2];
+	double** Bdbl = new double* [n];
+	for (int i = 0; i < n; ++i)
+		Bdbl[i] = new double[n2];
+	double** Cdbl = new double* [10];
+	for (int i = 0; i < 10; ++i)
+		Cdbl[i] = new double[n2];
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n2; ++j)
+		{
+			Bsgl[i][j] = rand();
+			Bdbl[i][j] = rand();
+		}
+	s1 = 0;
+	t1 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s1)
+		for (int i = 0; i < n2; ++i) {
+			for (int j = 0; j < n; ++j) {
+				s1 += Asgl[j] * Bsgl[j][i]; cout << "";
+			}
+			Csgl[0][i] = s1;
+			s1 = 0;
+		}
+	}
+	t2 = clock();
+	cout << "Время умножения матриц float: " << t2 - t1 << "\n";
+	s2 = 0;
+	t3 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s2)
+		for (int i = 0; i < n2; ++i) {
+			for (int j = 0; j < n; ++j) {
+				s2 += Adbl[j] * Bdbl[j][i]; std::cout << "";
+			}
+			Cdbl[0][i] = s2;
+			s2 = 0;
+		}
+	}
+	t4 = clock();
+	cout << "Время умножения матриц double: " << t4 - t3 << "\n\n";
 }

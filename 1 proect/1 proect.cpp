40b69bb55bcc3a18885b31bde1ffd1189b29a1f1
maksimvedeshin <Vedeshinm@gmail.com>
2023-06-27 14:15:22 +1000

    ez

--- 1 proect/1 proect.cpp
+++ 1 proect/1 proect.cpp
@@ -1,9 +1,46 @@
 ﻿#include <iostream>
+#include <iostream>
 #include <omp.h>
 #include <ppl.h>
 using namespace std;
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	setlocale(LC_ALL, "rus");
+	cout << "Количество доступных потоков: " << omp_get_num_procs() << "\n";
+	const int n = 10e6;
+	float* Asgl = new float[n];
+	double* Adbl = new double[n];
+	for (int i = 0; i < n; ++i)
+	{
+		Asgl[i] = rand();
+		Adbl[i] = rand();
+	}
+	float s1 = 0;
+	double s2 = 0;
+	int threads = 32;
+	omp_set_num_threads(threads);
+	cout << "Количество потоков: " << threads << "\n";
+	unsigned int t1, t2, t3, t4;
+	t1 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s1)
+		for (int i = 0; i < n; ++i)
+			s1 += Asgl[i];
+	}
+	t2 = clock();
+	cout << "Время суммирования массива float: " << t2 - t1 << "\n";
+	t3 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s2)
+		for (int i = 0; i < n; ++i)
+			s2 += Asgl[i];
+	}
+	t4 = clock();
+	cout << "Время суммирования массива double: " << t4 - t3 << "\n";
 	const int n2 = 2;
 	float** Bsgl = new float* [n];
 	for (int i = 0; i < n; ++i)
